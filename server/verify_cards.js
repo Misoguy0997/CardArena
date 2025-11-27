1c853607c9017cfc53ee15e3022309f5919ec03a
Misoguy <itemwh7542@gmail.com>
2025-11-27 12:03:04 +0900

    Fix: Add .gitignore and remove node_modules from git

--- server/verify_cards.js
+++
@@ -1,9 +0,0 @@
-const { getInitialDeck, getCardById } = require('./cardData');
-
-console.log('--- Verifying Card Data ---');
-const deck = getInitialDeck();
-const warrior = deck.find(c => c.name === 'Warrior');
-console.log('Warrior from getInitialDeck:', warrior);
-
-const warriorById = getCardById(1);
-console.log('Warrior from getCardById:', warriorById);
